f8bdd9a368aede8484d7aa445919eddeeac5e64d
unknown <rithlesscorp@gmail.com>
2025-04-22 13:51:30 +0200

    a little cleaning routine

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Counter::class);
+Route::get('/', function () {
+    return view('welcome');
+});
