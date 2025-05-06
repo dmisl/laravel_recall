47aa411f8734f88ad97d9783f23fb0d59c96a1dd
unknown <rithlesscorp@gmail.com>
2025-05-06 14:14:26 +0200

    feat(Product): add product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public $fillable = [
+        'title', 'price', 'description', 'image', 'category'
+    ];
+}
