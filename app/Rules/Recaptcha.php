914638f285e37d4b4c00e254efe79086a14b9015
unknown <rithlesscorp@gmail.com>
2025-05-17 03:31:00 +0200

    feat(Recaptcha, RegisterRequest): implement simple rules for sign up with google api bot validation

--- app/Rules/Recaptcha.php
+++ app/Rules/Recaptcha.php
@@ -4,16 +4,21 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use App\Services\RecaptchaService;
 
 class Recaptcha implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
+    protected RecaptchaService $service;
+
+    public function __construct(RecaptchaService $service)
+    {
+        $this->service = $service;
+    }
+
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        if (!$this->service->verify($value)) {
+            $fail('Не вдалося пройти перевірку reCAPTCHA.');
+        }
     }
 }
